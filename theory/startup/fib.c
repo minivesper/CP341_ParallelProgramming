a969342896d12ecc0a6add838ee65e853099191f
minivesper <minivesper@live.com>
2017-09-26 12:02:05 -0600

    speedup

--- theory/startup/fib.c
+++ theory/startup/fib.c
@@ -5,13 +5,13 @@ int fib(int num1, int num2, int start, int fin) {
     int num3;
     num3 = num1+num2;
     printf("%d",num3);
-    printf("\n");
+    printf(" ");
     fib(num2,num3,start+1,fin);
   }
   return 0;
 }
 
 int main(int argc, char const *argv[]) {
-  fib(0,1,0,5);
+  fib(0,1,0,10);
   return 0;
 }
